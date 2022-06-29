dd13a436dcd151c29e40aa95fd5ef5c8421b0478
karl-no <karl.publicidade@gmail.com>
2022-06-29 18:02:37 -0300

    Requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (employeeName === undefined) {
+    return {};
+  }
+  return data.employees.find(({ firstName, lastName }) => firstName === employeeName
+   || lastName === employeeName);
 }
+
+// console.log(getEmployeeByName());
 
 module.exports = getEmployeeByName;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -9,6 +9,6 @@ function getSpeciesByIds(...ids) {
   return species;
 }
 
-// console.log(getSpeciesByIds());
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 
 module.exports = getSpeciesByIds;
