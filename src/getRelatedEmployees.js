ce9c4639ea864166ebde4699577ba75655e49f9c
karl-no <karl.publicidade@gmail.com>
2022-06-29 20:50:17 -0300

    Requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,23 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  // retorna true se o id for de um manager
+  // return managers.some((manager) => manager === id);
+  return data.employees.some((callback) =>
+    callback.managers.some((manager) => manager === id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId)) {
+    const manager = data.employees.filter((callback) =>
+      callback.managers.some((id) => id === managerId));
+
+    return manager.map((employee) => `${employee.firstName} ${employee.lastName}`);
+  }
+
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
 }
 
 module.exports = { isManager, getRelatedEmployees };
