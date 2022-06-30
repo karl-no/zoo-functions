1c2354adb6b69f42b3a75f8dac2cfeef7310d427
karl-no <karl.publicidade@gmail.com>
2022-06-30 17:14:02 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,18 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  const animais = {};
+  if (!animal) {
+    data.species.forEach((info) => {
+      animais[info.name] = info.residents.length;
+    });
+    return animais;
+  }
+  if (!animal.sex) {
+    return data.species.find((animals) => animals.name === animal.specie).residents.length;
+  }
+  return data.species.find((animals) => animals.name === animal.specie)
+    .residents.filter((animals) => animals.sex === animal.sex).length;
 }
 
 module.exports = countAnimals;
