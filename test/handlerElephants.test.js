52a5224e81e3965521b63201cd9b9a5aa6a98dfa
karl-no <karl.publicidade@gmail.com>
2022-06-30 18:47:39 -0300

    Requisito 6

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,24 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  // Para o argumento count deve retornar o número inteiro 4;
+  it('Para o argumento count deve retornar o número inteiro 4', () => {
+    expect(handlerElephants('count')).toBe(4);
+  });
+  // Para o argumento names deve retornar um array de nomes que possui o nome Jefferson;
+  it('Para o argumento names deve retornar um array de nomes que possui o nome Jefferson', () => {
+    expect(handlerElephants('names')).toContain('Jefferson');
+  });
+  // Para o argumento averageAge deve retornar um número próximo a 10.5;
+  it('Para o argumento averageAge deve retornar um número próximo a 10.5', () => {
+    expect(handlerElephants('averageAge')).toBeCloseTo(10.5);
+  });
+  // Para argumento vazio deve retornar undefined
+  it('Para o argumento vazio deve retornar undefined', () => {
+    expect(handlerElephants()).toBeUndefined();
+  });
+  // Para argumento que não é uma string deve retornar: 'Parâmetro inválido, é necessário uma string'
+  it('Para argumento que não é uma string deve retornar: Parâmetro inválido, é necessário uma string', () => {
+    expect(handlerElephants(1)).toBe('Parâmetro inválido, é necessário uma string');
+  });
+});
