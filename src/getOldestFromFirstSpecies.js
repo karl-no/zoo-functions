fa0287e8fed234356bdbb7ab4427f03f8eae6cd2
karl-no <karl.publicidade@gmail.com>
2022-07-01 16:03:56 -0300

    Requisitos 12 e 13

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -12,6 +12,6 @@ function getOldestFromFirstSpecies(id) {
   }));
 }
 
-console.log(getOldestFromFirstSpecies('56d43ba3-a5a7-40f6-8dd7-cbb05082383f'));
+// console.log(getOldestFromFirstSpecies('56d43ba3-a5a7-40f6-8dd7-cbb05082383f'));
 
 module.exports = getOldestFromFirstSpecies;
