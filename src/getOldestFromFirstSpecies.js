4a5491f6cc637cfe0384a464fad371bc602838f8
karl-no <karl.publicidade@gmail.com>
2022-07-01 15:21:08 -0300

    Requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,17 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const animal = data.employees.find((employee) => employee.id === id).responsibleFor[0];
+  const animalResidents = data.species.find((specie) => specie.id === animal).residents;
+
+  return Object.values(animalResidents.reduce((acc, curr) => {
+    if (curr.age >= acc.age) {
+      return curr;
+    }
+    return acc;
+  }));
 }
+
+console.log(getOldestFromFirstSpecies('56d43ba3-a5a7-40f6-8dd7-cbb05082383f'));
 
 module.exports = getOldestFromFirstSpecies;
